909f25dbe78536d1279b0a17699c527908525f43
Burhanuddin Bhopalwala <bbhopalw+github@gmail.com>
2021-10-07 20:19:59 +0530

    add(tests/conftest.py): pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
+# Copyright 2021 Burhanuddin Bhopalwala
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-def pytest_configure(config):
+def pytest_configure(config) -> None:
+    '''Pytest session config.'''
     config.addinivalue_line(
-        "markers", "unittest: mark test as unit test"
+        'markers', 'bank_account_skipif: mark test as bank account skipif (select with \'pytest -m unittest\'.)'
     )
-    config.addinivalue_line(
-        "markers", "integrationtest: mark test as integration test"
-    )
-    config.addinivalue_line(
-        "markers", "datasciencetest: mark test as datascience test"
